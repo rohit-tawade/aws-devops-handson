ac1b5ba93a391a3346e6345d205e3f9b64a4818d
rohit-tawade <rohit.s.tawade@gmail.com>
2025-06-18 00:02:15 +0530

    Updated

--- Terraform/ecr-ecs/variables.tf
+++ Terraform/ecr-ecs/variables.tf
@@ -8,10 +8,12 @@ variable "app_name" {
 
 variable "subnet_ids" {
   type = list(string)
+  default = [ "subnet-0b68e9127af19b57d", "subnet-0f7ba6cc9e8ab61ae" ]
   description = "List of subnet IDs for ECS service networking"
 }
 
 variable "security_group_id" {
   type = string
+  default = "sg-0faf1c65cea866ed9"
   description = "Security group ID for ECS service networking"
 }
